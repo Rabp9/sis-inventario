1f3a411eeda02cd50e693943917b05d4b41ae7ea
tmtrbocanegra <tmtrbocanegra@DESKTOP-PTTSRKA.TMT.LOCAL>
2017-11-20 09:02:27 -0500

    Avanzando

--- app/scripts/controllers/bienes.js
+++ app/scripts/controllers/bienes.js
@@ -9,31 +9,60 @@
  */
 angular.module('sisInventarioFrontendApp')
 .controller('BienesCtrl', function ($scope, BienesService, $uibModal, $utilsViewService) {
-    $scope.maxSize = 10;
+    
+    $scope.search = {};
+    $scope.search.text = '';
     $scope.page = 1;
-    $scope.loading = false;
+    $scope.maxSize = 10;
     
     $scope.getBienes = function() {
+        $scope.bienes = [];
         $scope.loading = true;
-        var data = BienesService.get({
+        BienesService.getBienesMovimientos({
             maxSize: $scope.maxSize,
-            page: $scope.page
-        }, function () {
+            page: $scope.page,
+            search: $scope.search.text
+        }, function (data) {
             $scope.bienes = data.bienes;
             $scope.pagination = data.pagination;
-            $scope.loading = false;
-        }, function (err) {
-            $scope.message = err.data;
             $scope.loading = false;
         });
     };
     
     $scope.getBienes();
     
+    $scope.$watch('search.text', function(oldValue, newValue) {
+        $scope.page = 1;
+        $scope.getBienes();
+    });
+    
     $scope.$watch('maxSize', function(newValue, oldValue) {
         $scope.page = 1;
-        getBienes();
+        $scope.getBienes();
     });
+    
+    $scope.showBienesAsignar = function(bien_id, event) {
+        $utilsViewService.disable(event.currentTarget);
+        
+        var modalInstanceAsignar = $uibModal.open({
+            templateUrl: 'views/bienes-asignar.html',
+            controller: 'BienesAsignarCtrl',
+            backdrop: false,
+            size: 'lg',
+            resolve: {
+                bien_id: function() {
+                    return bien_id;
+                }
+            }
+        });
+        
+        $utilsViewService.enable(event.currentTarget);
+        
+        modalInstanceAsignar.result.then(function (data) {
+            $scope.getBienes();
+            $scope.message = data;
+        });
+    };
     
     $scope.showBienesAdd = function(event) {
         $utilsViewService.disable(event.currentTarget);
@@ -75,6 +104,7 @@ angular.module('sisInventarioFrontendApp')
     };
     
     $scope.pageChanged = function() {
-        getBienes();
+        $scope.getBienes();
     };
+    
 });
